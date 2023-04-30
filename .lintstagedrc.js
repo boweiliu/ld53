faac0d02fc3baaa7d4570a367117914821d38774
Bowei Liu <liubowei@gmail.com>
2023-04-30 15:05:03 -0700

    add css

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 export default {
       '*.{ts,tsx,js,jsx}': 'eslint --cache --fix',
       '*.md': 'prettier --write',
+      '*.css': 'stylelint --cache --fix',
       '*.{ts,tsx}': () => 'tsc --skipLibCheck --noEmit',
 
 };
