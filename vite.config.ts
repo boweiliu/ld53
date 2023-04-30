1411403b55c69e017f711e176b3bdda914967cb5
Bowei Liu <liubowei@gmail.com>
2023-04-29 18:36:25 -0700

    try config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: './',
   test: {
     globals: true,
