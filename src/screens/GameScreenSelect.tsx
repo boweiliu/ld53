cf98794b0c373d672a065364b495db7ea78757ca
Bowei Liu <liubowei@gmail.com>
2023-04-30 16:16:52 -0700

    rotation done!

--- src/screens/GameScreenSelect.tsx
+++ src/screens/GameScreenSelect.tsx
@@ -1,16 +1,37 @@
 import { useState } from 'react';
+import classnames from 'classnames';
 import { RotationCheck } from '@/screens/RotationCheck';
+import styles from './GameScreenSelect.module.css';
 
 export function GameScreenSelect(props: {
-  size: { isLandscape: boolean };
+  size: { isLandscape: boolean; width: number; height: number };
   setSize: () => void;
 }) {
+  const {
+    size: { isLandscape, width, height },
+  } = props;
   const [screenState, setScreenState] = useState<string>('rotationCheck');
 
   return (
     <>
       {screenState === 'rotationCheck' ? (
         <RotationCheck {...props} onStart={() => setScreenState('startMenu')} />
+      ) : null}
+
+      {screenState !== 'rotationCheck' ? (
+        <div
+          className={classnames(
+            styles.rotationContainer,
+            isLandscape ? undefined : styles.rotate
+          )}
+          style={{
+            width: isLandscape ? width : height,
+            height: isLandscape ? height : width,
+            top: isLandscape ? 0 : height,
+          }}
+        >
+          hello world
+        </div>
       ) : null}
     </>
   );
