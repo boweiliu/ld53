22d6a4bddac4f33b9ea7adb27ba068b172d4c5e4
Bowei Liu <liubowei@gmail.com>
2023-04-29 20:27:17 -0700

    test sandbox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
+function testSandbox() {
+  // fetch? no - is blocked
+  try {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => console.log('fetched', json));
+  } catch (e: unknown) {
+    console.log(e, (e as Error)?.stack);
+  }
+
+  try {
+    localStorage.setItem('test', 'foo');
+    console.log('got local storage', localStorage.getItem('test'));
+  } catch (e: unknown) {
+    console.log(e, (e as Error)?.stack);
+  }
+}
+
 function Home() {
   console.log('hi there!');
 
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response) => response.json())
-    .then((json) => console.log('fetched', json));
+  testSandbox();
 
   return (
     <div style={{ backgroundColor: 'red' }}>
